92700c7a40794841e557aa7bf7a5ee4d09b0396e
dh-sato <42200732+dh-sato@users.noreply.github.com>
2019-01-18 14:32:57 +0900

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+deploy_path="/home/webapp/laravelsample"
+
+cd ${deploy_path} || exit 99
+
 cp -p .env.example .env
 php artisan key:generate
 
@@ -7,9 +11,9 @@ php artisan view:clear
 php artisan cache:clear
 composer dump-autoload --optimize
 
-chmod 777 -R ./storage
-chmod 777 -R ./vender
-chown -R nginx ./*
+chmod -R 777 ./storage
+chmod -R 777 ./vender
+chown -R nginx. ./*
 
 systemctl restart php-fpm.service
 systemctl restart nginx.service
