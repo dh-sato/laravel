07400f3d66ae07e6eba629db19d065785ce3912f
dh-sato <42200732+dh-sato@users.noreply.github.com>
2019-01-18 12:11:23 +0900

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-yum -y install nginx
-systemctl start nginx
+cp -p .env.example .env
+php artisan key:generate
+
+php artisan storage:link
+php artisan view:clear
+php artisan cache:clear
+composer dump-autoload --optimize
+
+chmod 777 -R ./storage
+chmod 777 -R ./vender
+chown -R nginx ./*
+
+systemctl restart php-fpm.service
+systemctl restart nginx.service
