9207ba12f4f06fb84162da32d40cfa612653148d
dh-sato <42200732+dh-sato@users.noreply.github.com>
2019-01-21 03:02:51 +0900

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         <!-- Styles -->
         <style>
             html, body {
-                background-color: #00B8DD;
+                background-color: #dd3e00;
                 color: #636b6f;
                 font-family: 'Raleway', sans-serif;
                 font-weight: 100;
