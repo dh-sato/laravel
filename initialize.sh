5c711b968b585c8c6c41df09795d536ece501fef
dh-sato <42200732+dh-sato@users.noreply.github.com>
2019-03-04 01:12:09 +0900

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
-deploy_path="/home/webapp/laravelsample"
+DEPLOY_PATH="/home/webapp/laravelsample"
 
-cd ${deploy_path} || exit 99
+cd ${DEPLOY_PATH} || exit 99
+
+sudo chown -R nginx:nginx ${DEPLOY_PATH}
+sudo chmod 2775 ${DEPLOY_PATH}
+sudo find ${DEPLOY_PATH} -type d -exec sudo chmod 2775 {} +
+sudo find ${DEPLOY_PATH} -type f -exec sudo chmod 0664 {} +
 
 cp -p .env.example .env
-php artisan key:generate
-
-php artisan storage:link
-php artisan view:clear
-php artisan cache:clear
 
 composer install
+composer dump-autoload
 
-chmod -R 777 ./storage
-chmod -R 777 ./vendor
-chown -R nginx. ./*
+php artisan key:generate
+php artisan storage:link
+php artisan optimize:clear
 
-systemctl restart php-fpm.service
-systemctl restart nginx.service
+sudo chmod -R 777 ./storage
+sudo chmod -R 777 ./vendor
+
+sudo systemctl restart php-fpm.service
+sudo systemctl restart nginx.service
